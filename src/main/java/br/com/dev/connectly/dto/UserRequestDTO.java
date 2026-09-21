a84145579617ced4565ce347d467ddf214893f41
GabrielDOSilva <gabriel.silva781.09@gmail.com>
2026-09-21 19:22:39 -0300

    Enforce strong password validation

--- src/main/java/br/com/dev/connectly/dto/UserRequestDTO.java
+++ src/main/java/br/com/dev/connectly/dto/UserRequestDTO.java
@@ -2,6 +2,7 @@ package br.com.dev.connectly.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class UserRequestDTO {
@@ -16,6 +17,10 @@ public class UserRequestDTO {
 	
 	@NotBlank
 	@Size(min = 8, max = 72)
+	@Pattern(
+	        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).+$",
+	        message = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"
+	    )
 	private String password;
 	
 	
